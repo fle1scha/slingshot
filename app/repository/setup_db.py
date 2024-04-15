9a6def26757690e609fe6ba5e3bef5c9e0e66df1
Antony Fleischer <ant.fleischer@gmail.com>
2024-04-14 22:07:09 -0700

    Update setup_db

--- app/repository/setup_db.py
+++ app/repository/setup_db.py
@@ -1,6 +1,5 @@
 import pymysql
 import config
-import logging
 
 # SQL Statements
 CREATE_DATABASE_SQL = f"CREATE DATABASE IF NOT EXISTS {config.DB_NAME};"
@@ -19,27 +18,25 @@ CREATE TABLE IF NOT EXISTS `{config.DB_NAME}`.`users` (
 );
 """
 
-# Execute a SQL command and handle any MySQL errors.
-def run_sql_command(sql):
-    connection = pymysql.connect(
-        host=config.MYSQL_HOST,
-        user=config.MYSQL_USER,
-        password=config.MYSQL_PASSWORD,
-        charset=config.MYSQL_CHARSET
-    )
+def run_sql_command(sql_command, logger):
     try:
-        with connection.cursor() as cursor:
-            cursor.execute(sql)
-        connection.commit()
-        return None  # Return None if the operation is successful
+        with pymysql.connect(
+            host=config.MYSQL_HOST,
+            user=config.MYSQL_USER,
+            password=config.MYSQL_PASSWORD,
+            charset=config.MYSQL_CHARSET,
+            autocommit=True
+        ) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(sql_command)
     except pymysql.MySQLError as e:
-        error_message = f"Error with run_sql_command: {e}"
-        return error_message
+        logger.error(f"Error executing SQL command: {e}")
+        return str(e)
+    return None
 
-# Set up the database, create the user, grant privileges, and create the users table.
 def setup_database(logger):
     # Create the database
-    database_error = run_sql_command(CREATE_DATABASE_SQL)
+    database_error = run_sql_command(CREATE_DATABASE_SQL, logger)
     if database_error:
         logger.error(database_error)
         return
@@ -47,12 +44,12 @@ def setup_database(logger):
     logger.info(f"Database '{config.DB_NAME}' created or already exists.")
 
     # Create the user and grant privileges
-    user_error = run_sql_command(CREATE_USER_SQL)
+    user_error = run_sql_command(CREATE_USER_SQL, logger)
     if user_error:
         logger.error(user_error)
         return
 
-    privileges_error = run_sql_command(GRANT_PRIVILEGES_SQL)
+    privileges_error = run_sql_command(GRANT_PRIVILEGES_SQL, logger)
     if privileges_error:
         logger.error(privileges_error)
         return
@@ -60,12 +57,9 @@ def setup_database(logger):
     logger.info(f"User '{config.DB_USER}' created or already exists, and privileges granted.")
 
     # Create the table(s)
-    table_error = run_sql_command(CREATE_TABLE_USERS_SQL)
+    table_error = run_sql_command(CREATE_TABLE_USERS_SQL, logger)
     if table_error:
         logger.error(table_error)
         return
 
     logger.info("Table 'users' created or already exists in the database.")
-
-if __name__ == '__main__':
-    setup_database()
