be88e40cc134cf726c430b69ed3c0a0264e4c42a
fle1scha <ant.fleischer@gmail.com>
2024-04-19 11:14:05 -0700

    Add name input field

--- app/service/broadcaster.py
+++ app/service/broadcaster.py
@@ -43,12 +43,12 @@ class Broadcaster:
             success, error_message = self.repository.insert_new_user(name, phone_number)
             if not success:
                 if error_message == 'already_registered':
-                    self.logger.info(f"{phone_number} already in database. Error: {error_message}")
+                    self.logger.info(f"{phone_number} under name {name} already in database. Error: {error_message}")
                     return False, 'already with us.'
                 else:
-                    self.logger.error(f"Failed to add user with phone number {phone_number} to the database. Error: {error_message}")
+                    self.logger.error(f"Failed to add {name} with phone number {phone_number} to the database. Error: {error_message}")
                     return False, 'error. please try again later'
-                
+
             # In local, don't hit Twilio messages.create API
             if Config.ENVIRONMENT != 'development':
                 try:
@@ -57,17 +57,15 @@ class Broadcaster:
                         from_=Config.TWILIO_PHONE_NUMBER,
                         to=phone_number
                     )
-                    self.logger.info(f"twilioClient.message.create initiated to {phone_number}. message sid: {message.sid}")
+                    self.logger.info(f"twilioClient.message.create initiated to {name} on {phone_number}. message sid: {message.sid}")
                     return True, ""
-                
                 except TwilioRestException as e:
                     error_message = str(e)
-                    self.logger.error(f"Message send failed to {phone_number}. Error: {error_message}")
+                    self.logger.error(f"Message send failed to {name} with number {phone_number}. Error: {error_message}")
                     return False, 'error. please try again later'
             else:
                 self.logger.info(f"Skipping Twilio API call in {Config.ENVIRONMENT} environment")
                 return True, ""
-
         except Exception as e:
-            self.logger.error(f"Unexpected error occurred while sending message to {phone_number}. Error: {str(e)}")
+            self.logger.error(f"Unexpected error occurred while sending message to {name} with number {phone_number}. Error: {str(e)}")
             return False, 'error. please try again later'
--- app/static/script.js
+++ app/static/script.js
@@ -7,30 +7,33 @@ const logoContainer = document.getElementById('logo-container')
 const responseMessage = document.getElementById('response-message');
 const validationMessage = document.getElementById('validation-message');
 const errorMessage = document.getElementById('error-message');
+const nameInput = document.getElementById('name');
+const phoneNumberInput = document.getElementById('phone_number');
 
 // Add an event listener to the form's submit event
 form.addEventListener('submit', async (event) => {
     event.preventDefault(); // Prevent the default form submission
 
-    const phoneNumber = form.elements.phone_number.value;
+    const name = nameInput.value.trim();
+    const phoneNumber = phoneNumberInput.value.trim();
 
-    // Check if the phone number is valid
-    if (isValidPhoneNumber(phoneNumber)) {
+    // Check if the name and phone number are valid
+    if (isValidName(name) && isValidPhoneNumber(phoneNumber)) {
         try {
             const response = await fetch('/', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded'
                 },
-                body: new URLSearchParams({ phone_number: phoneNumber })
+                body: new URLSearchParams({ name, phone_number: phoneNumber })
             });
 
             const data = await response.json();
 
             if (data.success) {
-                    logoContainer.style.display = 'none';
-                    formGroup.style.display = 'none';                
-                    successTitle.style.display = 'block';
+                logoContainer.style.display = 'none';
+                formGroup.style.display = 'none';
+                successTitle.style.display = 'block';
             } else {
                 // Display the error message
                 if (errorMessage) {
@@ -46,7 +49,6 @@ form.addEventListener('submit', async (event) => {
                 }
             }
         } catch (error) {
-
             // Display a generic error message for other errors
             if (errorMessage) {
                 errorMessage.textContent = 'An unexpected error occurred. Please try again later.';
@@ -59,8 +61,14 @@ form.addEventListener('submit', async (event) => {
             }
         }
     } else {
-        // Display the custom validation error message
-        validationMessage.style.display = 'block';
+        // Display the custom validation error messages
+        if (!isValidName(name)) {
+            validationMessage.textContent = 'Enter a valid name.';
+            validationMessage.style.display = 'block';
+        } else {
+            validationMessage.textContent = 'Enter a 10-digit phone number.';
+            validationMessage.style.display = 'block';
+        }
 
         // Hide the validation message after 1.5 seconds
         setTimeout(() => {
@@ -73,4 +81,9 @@ function isValidPhoneNumber(phoneNumber) {
     // Validate the phone number format
     const phoneNumberRegex = /^\d{10}$/;
     return phoneNumberRegex.test(phoneNumber);
+}
+
+function isValidName(name) {
+    // Validate the name (not empty)
+    return name.length >= 3 && name.length < 51;
 }
