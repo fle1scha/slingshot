de0b421d2a4478d29f9f484995732337ced854c2
fle1scha <ant.fleischer@gmail.com>
2024-05-18 15:20:04 -0700

    Add new path

--- app/views.py
+++ app/views.py
@@ -1,4 +1,3 @@
-# app/views.py
 from flask import request, jsonify, render_template
 from app import app, broadcaster
 
@@ -7,7 +6,7 @@ def index():
     if request.method == 'POST':
         input_phone_number = request.form['phone_number']
         input_name = request.form['name']
-        welcome_message = 'slingshot is a community adventure project.\n\nwe will text you about races, gatherings, and adventures. next race 05/17.'
+        welcome_message = 'slingshot is a community adventure project.\n\nwe will text you about races, gatherings, and adventures.'
         success, error_message = broadcaster.send_message(input_name, input_phone_number, welcome_message)
         
         if success:
@@ -16,3 +15,12 @@ def index():
             return jsonify({'error': True, 'error_message': error_message}), 400
         
     return render_template('index.html')
+
+@app.route('/cop', methods=['GET', 'POST'])
+def cop_view():
+    if request.method == 'POST':
+        # Process the form data from the '/cop' route
+        # You can add your custom logic here
+        return jsonify({'success': True})
+    
+    return render_template('cop.html')
