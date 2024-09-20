f0576de76c548602c6d0f162f8da49d5bb1a95f0
fle1scha <ant.fleischer@gmail.com>
2024-09-19 21:05:52 -0700

    Update views.py

--- app/views.py
+++ app/views.py
@@ -7,7 +7,7 @@ def index():
     if request.method == 'POST':
         input_phone_number = request.form['phone_number']
         input_name = request.form['name']
-        welcome_message = 'slingshot is a community adventure project. we will text you soon.'
+        welcome_message = "slingshot is a community adventure project. bike 'race'; 27/09, 18:00; location & format next week. off-road bikes encouraged, but never enforced.\n\nreply MORPH to receive updates for this event."
         success, error_message = broadcaster.send_message(input_name, input_phone_number, welcome_message)
 
         if success:
@@ -17,6 +17,3 @@ def index():
 
     return render_template('index.html')
 
-@app.route('/fuzz', methods=['GET'])
-def fuzz_project():
-    return render_template('fuzz.html') 
