b88d6de719078a22304de757ba9a2d55009f2dee
fle1scha <ant.fleischer@gmail.com>
2025-02-06 10:18:44 -0800

    update are.na service

--- app/service/arena.py
+++ app/service/arena.py
@@ -1,21 +1,24 @@
 import requests
-from config import Config
+import random
 
 class ArenaService:
     def __init__(self, channel_slug='left-coast'):
         """Initialize with the channel slug."""
-        self.api_url = f'http://api.are.na/v2/channels/{channel_slug}/contents?per=10'
+        self.channel_slug = channel_slug
 
     def fetch_images(self):
-        """Fetch images from the Are.na channel."""
+        """Fetch images from the Are.na channel with a random page number."""
+        page_number = random.randint(1, 5)  # Adjust range as needed
+        api_url = f'http://api.are.na/v2/channels/{self.channel_slug}/contents?per=7&page={page_number}'
+
         try:
-            response = requests.get(self.api_url)
+            response = requests.get(api_url)
             response.raise_for_status()
             data = response.json()
 
             # Filter out the contents that are images
             images = [
-                item['image']['display'] for item in data['contents'] if 'image' in item
+                item['image']['display'] for item in data.get('contents', []) if 'image' in item
             ]   
             print(images)
             return images
